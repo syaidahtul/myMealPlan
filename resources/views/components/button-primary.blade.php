aac5e7353f0ae3d55e33235bfe9b3a5c713644a9
syaidahtul <azizsyaidahtul@gmail.com>
2024-07-25 17:56:54 +0800

    DEV|button styling

--- resources/views/components/button-primary.blade.php
+++ resources/views/components/button-primary.blade.php
@@ -2,7 +2,6 @@
     'disabled' => false,
 ])
 
-<button
-    {{ $attributes->class(['btn btn-primary fw-normal text-uppercase shadow-sm'])->merge(['type' => 'button'])->merge(['disabled' => false]) }}>
+<button {{ $attributes->class(['btn btn-primary'])->merge(['type' => 'button'])->merge(['disabled' => false]) }}>
     {{ $slot }}
 </button>
